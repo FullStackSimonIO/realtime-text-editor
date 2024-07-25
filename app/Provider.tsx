6448bb9dd8bcab5608cad9c188ba311895ef2df4
FullStackSimon <fullstacksimon@gmail.com>
2024-07-25 19:52:56 +0200

    added providers

---
+++ app/Provider.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React, { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+import Loader from "@/components/Loader";
+
+const Provider = ({ children }: { children: ReactNode }) => {
+  return (
+    <div>
+      <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+        <ClientSideSuspense fallback={<Loader />}>
+          {children}
+        </ClientSideSuspense>
+      </LiveblocksProvider>
+    </div>
+  );
+};
+
+export default Provider;
